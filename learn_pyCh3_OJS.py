b50c50c79568eadf54843d8736b68f53d1b30bcf
Junsesoon <junsesoon@gmail.com>
2021-10-25 18:47:29 +0900

    Python Ch3 Change the order of tasts to your liking - if(), while, for, range, random

---
+++ learn_pyCh3_OJS.py
@@ -0,0 +1,23 @@
+for i in range(2, 10) :
+    print('----{}단'.format(i))
+
+    for j in range(1, 10) : \
+        print('%d * %d = %d'%(i,j,i*j))
+
+string = """나는 홍길동 입니다.
+주소는 서울시 입니다.
+나이는 35세 입니다."""
+
+sents = []
+words = []
+
+for sen in string.split(sep = "\n") :
+    sents.append(sen)
+
+    for word in sen.split() :
+        words.append(word)
+
+print("문장 : ", sents)
+print("문장수 : ", len(sents))
+print("단어 : ", words)
+print("단어수 : ", len(words))
